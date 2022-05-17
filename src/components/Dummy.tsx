077b26049f0c9281cefd704ee2db7d91ed16bcd8
taishi1116 <shinoda1116.miwa@gmail.com>
2022-05-17 22:12:46 +0900

    feat: 仮コンポーネントの実装

---
+++ src/components/Dummy.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DummyComponent = () => {
+  return <div>Dummy</div>;
+};
---
+++ src/pages/dummy.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { DummyComponent } from "../components/Dummy";
+
+export const Dummy = () => {
+  return <DummyComponent />;
+};
