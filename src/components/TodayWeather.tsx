bf19e348076fee7e0d395aabd6a696ab1a86b657
笠井　健太 <kentakasai@This-is-kentas-MacBook-Air-device.local>
2022-05-25 14:30:19 +0900

    本日の天気コンポーネントの実装

---
+++ src/components/TodayWeather.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type Props = {
+	iconUrl: string;
+	todayTemperature: number;
+	unit: string;
+	todayWeather: string;
+};
+
+export const TodayWeather: React.FC<Props> = ({
+	iconUrl,
+	todayTemperature,
+	unit,
+	todayWeather,
+}) => {
+	return (
+		<div className="h-screen bg-navy text-center">
+			<div className="relative flex flex-nowrap top-32 justify-center top-38 items-center max-h-40">
+				<img className="h-48 p-4 m-8" src={iconUrl} />
+			</div>
+			<div className="flex flex-col items-center pt-6">
+				<h1 className="relative text-[100px] flex-5 py-8 h-56 w-48 m-10">
+					{todayTemperature}
+					<span className="text-5xl text-gray-250">&deg;{unit}</span>
+				</h1>
+			</div>
+			<div className="h-52">
+				<h3 className="font-semibold text-4xl text-gray-250">{todayWeather}</h3>
+			</div>
+			<div className="flex flex-col items-center text-center text-gray-350 text-lg space-y-5">
+				<p>Today &bull; Fri 5 Jun</p>
+				<p>
+					<i className="fas fa-map-marker-alt"></i> Helsinki
+				</p>
+			</div>
+		</div>
+	);
+};
---
+++ src/stories/TodayWeather.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { TodayWeather } from "../components/TodayWeather";
+
+export default {
+	title: "TodayWeather",
+	component: TodayWeather,
+} as ComponentMeta<typeof TodayWeather>;
+
+const Template: ComponentStory<typeof TodayWeather> = (args) => (
+	<div className="w-1/3 h-screen text-white">
+		<TodayWeather {...args} />
+	</div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+	iconUrl: "https://www.jma.go.jp/bosai/forecast/img/212.svg",
+	todayTemperature: 15,
+	unit: "C",
+	todayWeather: "Shower",
+};
