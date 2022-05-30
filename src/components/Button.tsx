1dc3e6af74af79f29c0c80c3a98cfa1387b9939a
笠井　健太 <kentakasai@This-is-kentas-MacBook-Air-device.local>
2022-05-30 16:08:58 +0900

    ButtonComponent修正

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,9 +5,5 @@ type Props = {
 };
 
 export const Button: React.FC<Props> = ({ title }) => {
-	return (
-		<div className="relative flex justify-between mb-10">
-			<button className="bg-slate-600 w-40 h-10">{title}</button>
-		</div>
-	);
+	return <button className="bg-slate-600 w-40 h-10">{title}</button>;
 };
