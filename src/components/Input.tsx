073de32b92071d79909cb6f1745747c653869eab
笠井　健太 <kentakasai@This-is-kentas-MacBook-Air-device.local>
2022-06-02 15:28:41 +0900

    差分が出てこないためInputComponent修正の再度PR

---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Input: React.FC = () => {
+	return (
+		<label className="relative block">
+			<span className="absolute inset-y-0 left-0 flex items-center pl-2">
+				<img className="h-5 w-5 fill-slate-300" src="../images/297822.svg" />
+			</span>
+			<input
+				className="placeholder:italic placeholder:text-slate-400 block bg-slate-600 border border-slate-300 rounded-md py-4 pl-10 pr-20 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+				placeholder="Search location"
+				type="text"
+				name="search"
+			/>
+		</label>
+	);
+};
