aea893c407a469b845b22aa20dee203c22be6593
taishi1116 <shinoda1116.miwa@gmail.com>
2022-06-08 22:14:05 +0900

    tsconfig.jsonの設定でpath取得はsrcディレクトリ起点にした

--- src/components/Dummy.tsx
+++ src/components/Dummy.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { AreaCard } from "src/components/AreaCard";
 
 export const DummyComponent = () => {
-  return <div>Dummy
-
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </div>;
+  return (
+    <div>
+      Dummy
+      <AreaCard name="aaa" onClick={() => {}} />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
