8ee5f973a2cc9cceff870858b1d6a2c68873db05
笠井　健太 <kentakasai@This-is-kentas-MacBook-Air-device.local>
2022-06-10 22:35:30 +0900

    WIP:API実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 import { Dummy } from "./pages/dummy";
 
-function App() {
-  return <Dummy />;
-}
+type Data = {
+	aera: string;
+	publicTime: string;
+};
+
+export const App: React.FC = () => {
+	const urlAPI = "https://weather.tsukumijima.net/api/forecast";
+	const [datas, setDatas] = useState([]);
+	console.log(axios.defaults.baseURL);
+	useEffect(() => {
+		axios.get(urlAPI).then((res) => {
+			setDatas(res.data);
+		});
+	}, []);
+	console.log(datas);
+	return (
+		<div>
+			<h1>Axios Test</h1>
+			<div>
+				{datas.map((data: Data) => (
+					<div>
+						{data.aera}:{data.publicTime}
+					</div>
+				))}
+				<Dummy />
+			</div>
+		</div>
+	);
+};
 
 export default App;
